3f21f5d9700d0d2cd063c3d3cef9ea5b866a16ed
frederik-kofoed-marqversen <frederik@marqversen.dk>
2024-01-13 18:55:29 +0100

    Applied formatting

--- utils/src/graph.rs
+++ utils/src/graph.rs
@@ -1,9 +1,13 @@
-use std::collections::{HashMap, BinaryHeap};
-use std::hash::Hash;
+use std::collections::{BinaryHeap, HashMap};
 use std::fmt::Debug;
+use std::hash::Hash;
 
-#[derive(Debug)]
-pub struct Graph<T, U> where T: Clone + Hash + Eq, U: Default + Clone {
+#[derive(Debug, Clone)]
+pub struct Graph<T, U>
+where
+    T: Clone + Hash + Eq,
+    U: Default + Clone,
+{
     // A weighted graph over nodes: T with edge weights: U.
     // For an undirected graph, simply let weights be unit: ().
     //     This is equivalent to using HashSet.
@@ -11,10 +15,16 @@ pub struct Graph<T, U> where T: Clone + Hash + Eq, U: Default + Clone {
 }
 
 // Graph manipulations
-impl<T, U> Graph<T, U> where T: Clone + Hash + Eq, U: Default + Clone {
+impl<T, U> Graph<T, U>
+where
+    T: Clone + Hash + Eq,
+    U: Default + Clone,
+{
     // Create new empty graph.
     pub fn new() -> Self {
-        Self { data: HashMap::new() }
+        Self {
+            data: HashMap::new(),
+        }
     }
 
     // Iterate through nodes in random order.
@@ -23,14 +33,19 @@ impl<T, U> Graph<T, U> where T: Clone + Hash + Eq, U: Default + Clone {
     }
 
     // Collects nodes into type A with: graph.nodes::<A>().
-    pub fn nodes<A: FromIterator<T>> (&self) -> A {
+    pub fn nodes<A: FromIterator<T>>(&self) -> A {
         self.data.keys().cloned().collect()
     }
 
-    pub fn neighbours<A: FromIterator<T>> (&self, node: &T) -> A {
-        self.data.get(node).unwrap_or(&HashMap::new()).keys().cloned().collect()
+    pub fn neighbours<A: FromIterator<T>>(&self, node: &T) -> A {
+        self.data
+            .get(node)
+            .unwrap_or(&HashMap::new())
+            .keys()
+            .cloned()
+            .collect()
     }
-    
+
     // Add node to graph. Do nothing if already present.
     pub fn add_node(&mut self, node: T) {
         self.data.entry(node).or_insert(HashMap::new());
@@ -39,7 +54,7 @@ impl<T, U> Graph<T, U> where T: Clone + Hash + Eq, U: Default + Clone {
     // Remove node and all its edges.
     pub fn remove_node(&mut self, node: T) {
         match self.data.remove(&node) {
-            None => {},
+            None => {}
             Some(neighbours) => {
                 for (neighbour, _) in neighbours {
                     self.remove_directed_edge(neighbour, node.clone());
@@ -51,12 +66,15 @@ impl<T, U> Graph<T, U> where T: Clone + Hash + Eq, U: Default + Clone {
     // Add edge from -> to. Create nodes if not present.
     pub fn add_directed_edge(&mut self, from: T, to: T, weight: Option<U>) {
         self.add_node(to.clone());
-        self.data.entry(from)
+        self.data
+            .entry(from)
             .or_insert(HashMap::new())
             .insert(to, weight.unwrap_or_default());
     }
     pub fn remove_directed_edge(&mut self, from: T, to: T) {
-        self.data.entry(from).and_modify(|x| {x.remove(&to);});
+        self.data.entry(from).and_modify(|x| {
+            x.remove(&to);
+        });
     }
 
     // Add edge u -> v and v -> u. Create nodes if not present.
@@ -70,42 +88,97 @@ impl<T, U> Graph<T, U> where T: Clone + Hash + Eq, U: Default + Clone {
     }
 }
 
-// Path finding
-#[derive(Debug, PartialEq, Eq)]
-struct State<T> {
-    node: T,
-    score: u64,
+impl<T, U> std::ops::Index<&T> for Graph<T, U>
+where
+    T: Clone + Hash + Eq,
+    U: Default + Clone,
+{
+    type Output = HashMap<T, U>;
+
+    #[inline(always)]
+    fn index(&self, key: &T) -> &Self::Output {
+        &self.data[key]
+    }
 }
 
-impl<T> PartialOrd for State<T> where T: PartialEq {
+// Path finding
+#[derive(Debug, PartialEq)]
+struct State<T, U> {
+    node: T,
+    score: U,
+}
+
+impl<T, U> Eq for State<T, U>
+where
+    T: PartialEq,
+    U: PartialEq,
+{
+    // This is a hack to get f64 to work. Eq is never used.
+}
+
+impl<T, U> PartialOrd for State<T, U>
+where
+    T: PartialEq,
+    U: PartialOrd,
+{
     fn partial_cmp(&self, other: &Self) -> Option<std::cmp::Ordering> {
         // The comparison is swapped to make State min-sorted
         other.score.partial_cmp(&self.score)
     }
 }
 
-impl<T> Ord for State<T> where T: Eq {
-    fn cmp(&self, other: &State<T>) -> std::cmp::Ordering {
+impl<T, U> Ord for State<T, U>
+where
+    T: Eq,
+    U: PartialOrd,
+{
+    fn cmp(&self, other: &State<T, U>) -> std::cmp::Ordering {
         self.partial_cmp(other).unwrap()
     }
 }
 
-impl<T> Graph<T, u64> where T: Clone + Hash + PartialOrd + Eq {
+impl<T, U> Graph<T, U>
+where
+    T: Clone + Hash + Eq,
+    U: Default + Clone,
+{
+    fn reconstruct_path<V>(data: &HashMap<T, (V, Option<T>)>, end: T) -> Vec<T> {
+        let mut path = Vec::from([end]);
+        while let Some(predecessor) = &data[path.last().unwrap()].1 {
+            path.push(predecessor.clone());
+        }
+        path.reverse();
+        return path;
+    }
+}
+
+impl<T> Graph<T, u64>
+where
+    T: Clone + Hash + PartialOrd + Eq,
+{
     // Returns the shortest path between start and end. Is None if no path exists.
-    pub fn a_star(&self, start: T, end: T, heuristic: Option<&dyn Fn(&T) -> u64>) -> Option<(u64, Vec<T>)> {
+    pub fn a_star(
+        &self,
+        start: T,
+        end: T,
+        heuristic: Option<&dyn Fn(&T) -> u64>,
+    ) -> Option<(u64, Vec<T>)> {
         // Without a heuristic this simply becomes Dijkstra's
         let heuristic = heuristic.unwrap_or(&|_| 0);
-        
+
         // data holds tuples (shortest_distance, Some(predecessor)).
         let mut data = HashMap::<T, (u64, Option<T>)>::from([(start.clone(), (0, None))]);
-        let mut queue = BinaryHeap::from([State{ node: start, score: 0 }]);
-        while let Some(state) = queue.pop() {            
+        let mut queue = BinaryHeap::from([State {
+            node: start,
+            score: 0,
+        }]);
+        while let Some(state) = queue.pop() {
             let current = state.node;
 
             if current == end {
                 let end_distance = data[&end].0;
                 let end_path = Self::reconstruct_path(&data, end);
-                return Some((end_distance, end_path))
+                return Some((end_distance, end_path));
             }
 
             for (next, weight) in &self.data[&current] {
@@ -113,28 +186,77 @@ impl<T> Graph<T, u64> where T: Clone + Hash + PartialOrd + Eq {
                 let d = data[&current].0 + weight;
                 // If entry does not exist, fill with default value.
                 let entry = data.entry(next.clone()).or_insert((u64::MAX, None));
-                
-                if d < entry.0 { // This path is shorter than previous best.
+
+                if d < entry.0 {
+                    // This path is shorter than previous best.
                     entry.0 = d;
                     entry.1 = Some(current.clone());
-                    queue.push(State { node: next.clone(), score: d + heuristic(next) });
+                    queue.push(State {
+                        node: next.clone(),
+                        score: d + heuristic(next),
+                    });
                 }
             }
         }
-        return None
-    }
-
-    fn reconstruct_path(data: &HashMap::<T, (u64, Option<T>)>, end: T) -> Vec<T> {
-        let mut path = Vec::from([end]);
-        while let Some(predecessor) = &data[path.last().unwrap()].1 {
-            path.push(predecessor.clone());
-        }
-        path.reverse();
-        return path
+        return None;
     }
 }
 
-impl<T, U> Graph<T, U> where T: Debug + Clone + Hash + Eq, U: Default + Clone {
+impl<T> Graph<T, f64>
+where
+    T: Clone + Hash + PartialOrd + Eq,
+{
+    // Returns the shortest path between start and end. Is None if no path exists.
+    pub fn a_star(
+        &self,
+        start: T,
+        end: T,
+        heuristic: Option<&dyn Fn(&T) -> f64>,
+    ) -> Option<(f64, Vec<T>)> {
+        // Without a heuristic this simply becomes Dijkstra's
+        let heuristic = heuristic.unwrap_or(&|_| 0.0);
+
+        // data holds tuples (shortest_distance, Some(predecessor)).
+        let mut data = HashMap::<T, (f64, Option<T>)>::from([(start.clone(), (0.0, None))]);
+        let mut queue = BinaryHeap::from([State {
+            node: start,
+            score: 0.0,
+        }]);
+        while let Some(state) = queue.pop() {
+            let current = state.node;
+
+            if current == end {
+                let end_distance = data[&end].0;
+                let end_path = Self::reconstruct_path(&data, end);
+                return Some((end_distance, end_path));
+            }
+
+            for (next, weight) in &self.data[&current] {
+                // Distance to next through current.
+                let d = data[&current].0 + weight;
+                // If entry does not exist, fill with default value.
+                let entry = data.entry(next.clone()).or_insert((f64::INFINITY, None));
+
+                if d < entry.0 {
+                    // This path is shorter than previous best.
+                    entry.0 = d;
+                    entry.1 = Some(current.clone());
+                    queue.push(State {
+                        node: next.clone(),
+                        score: d + heuristic(next),
+                    });
+                }
+            }
+        }
+        return None;
+    }
+}
+
+impl<T, U> Graph<T, U>
+where
+    T: Debug + Clone + Hash + Eq,
+    U: Default + Clone,
+{
     pub fn max_flow() {
         // Edmond Karp: https://en.wikipedia.org/wiki/Ford%E2%80%93Fulkerson_algorithm
     }
@@ -142,6 +264,21 @@ impl<T, U> Graph<T, U> where T: Debug + Clone + Hash + Eq, U: Default + Clone {
     pub fn min_cut() {
         // Max-flow min-cut: https://en.wikipedia.org/wiki/Max-flow_min-cut_theorem
     }
+}
+
+pub fn grid_graph_2d(n: usize, m: usize) -> Graph<(usize, usize), u64> {
+    let mut graph = Graph::new();
+    for i in 0..n-1 {
+        for j in 0..m {
+            graph.add_edge((i, j), (i+1, j), Some(1));
+        }
+    }
+    for j in 0..m-1 {
+        for i in 0..n {
+            graph.add_edge((i, j), (i, j+1), Some(1));
+        }
+    }
+    return graph
 }
 
 #[cfg(test)]
@@ -156,29 +293,70 @@ mod tests {
         graph.add_edge(0, 1, None);
         graph.add_edge(1, 2, None);
         graph.remove_node(1);
-        
+
+        // Removing edges should not create nodes.
+        graph.remove_edge(0, 10);
+
         assert_eq!(graph.nodes::<HashSet<u8>>(), HashSet::from([0, 2]));
-        assert!(
-            graph.neighbours::<Vec<u8>>(&0).len() == 0 
-                && 
-            graph.neighbours::<Vec<u8>>(&2).len() == 0
-        )
+        assert_eq!(graph.neighbours::<Vec<u8>>(&0).len(), 0);
+        assert_eq!(graph[&2].len(), 0);
     }
 
     #[test]
-    fn dijkstra() {
-        let mut graph: Graph<u8, u64> = Graph::new();
-        graph.add_edge(0, 1, Some(12));
-        graph.add_edge(1, 2, Some(12));
-        graph.add_edge(2, 3, Some(12));
-        graph.add_edge(3, 6, Some(20));
-        graph.add_edge(0, 4, Some(12));
-        graph.add_edge(0, 5, Some(30));
-        graph.add_edge(5, 6, Some(40));
-        
+    fn grid_graph() {
+        let graph = grid_graph_2d(2, 3);
+
+        // Check nodes
         assert_eq!(
-            graph.a_star(0, 6, None), 
-            Some((56, vec![0, 1, 2, 3, 6]))
+            graph.nodes::<HashSet<_>>(),
+            HashSet::from([(0, 0), (0, 1), (0, 2), (1, 0), (1, 1), (1, 2)])
+        );
+
+        // Check connections
+        assert_eq!(
+            graph.neighbours::<HashSet<_>>(&(0, 0)),
+            HashSet::from([(0, 1), (1, 0)])
+        );
+        assert_eq!(
+            graph.neighbours::<HashSet<_>>(&(1, 2)),
+            HashSet::from([(0, 2), (1, 1)])
+        );
+        assert_eq!(
+            graph.neighbours::<HashSet<_>>(&(1, 1)),
+            HashSet::from([(0, 1), (1, 0), (1, 2)])
+        );
+    }
+
+    #[test]
+    fn a_star() {
+        let maze = [
+            [0, 0, 0, 1, 0],
+            [0, 1, 0, 0, 0],
+            [0, 1, 0, 1, 0],
+            [0, 1, 0, 1, 0],
+            [0, 0, 0, 1, 0],
+        ];
+        let n = maze.len();
+        let (start, end) = ((0, 0), (n-1, n-1));
+        
+        let mut graph = grid_graph_2d(n, n);
+        for (i, row) in maze.iter().enumerate() {
+            for (j, num) in row.iter().enumerate() {
+                if num == &1 {
+                    graph.remove_node((i, j));
+                }
+            }
+        }
+
+        // Manhattan distance to end
+        let heuristic = |pos: &(usize, usize)| (pos.0.abs_diff(end.0) + pos.1.abs_diff(end.1)) as u64;
+        let sol_dijkstra = graph.a_star(start, end, None);
+        let sol_a_star = graph.a_star(start, end, Some(&heuristic));
+
+        assert_eq!(sol_dijkstra, sol_a_star);
+        assert_eq!(
+            sol_a_star,
+            Some((8, vec![(0, 0), (0, 1), (0, 2), (1, 2), (1, 3), (1, 4), (2, 4), (3, 4), (4, 4)]))
         );
     }
 }
--- utils/src/utility_functions.rs
+++ utils/src/utility_functions.rs
@@ -1,17 +1,17 @@
 pub fn floyd<T: Clone + Eq>(x0: T, map: &dyn Fn(&T) -> T) -> (usize, usize) {
     // Floyd's cycle detection algorithm.
     // Returns tuple (cycle period, cycle start)
-   
+
     let mut tortoise = map(&x0);
     let mut hare = map(&map(&x0));
     while tortoise != hare {
-        tortoise = map(&tortoise);  // tortoise moves by one step
-        hare = map(&map(&hare));  // hare moves with double the speed
+        tortoise = map(&tortoise); // tortoise moves by one step
+        hare = map(&map(&hare)); // hare moves with double the speed
     }
 
     // If the position (number of steps taken) of the tortoise is `nu` then the position of the hare will be 2 * `nu`
     // Since they are the same: x(`nu`) = x(2 * `nu`), the position `nu` must be a multiple of the period `lambda`
-   
+
     // Let `mu` be the starting position of the cycle
     // Since `nu` = n * `lambda` we know that x(`mu`) = x(`mu` + 2*`nu`)
     // This can be found by advancing `mu` steps from x(0) and x(2*`nu`) simultaneously
@@ -21,7 +21,7 @@ pub fn floyd<T: Clone + Eq>(x0: T, map: &dyn Fn(&T) -> T) -> (usize, usize) {
     tortoise = x0;
     while tortoise != hare {
         tortoise = map(&tortoise);
-        hare = map(&tortoise);  // Same speed
+        hare = map(&tortoise); // Same speed
         mu += 1;
     }
 
@@ -34,41 +34,41 @@ pub fn floyd<T: Clone + Eq>(x0: T, map: &dyn Fn(&T) -> T) -> (usize, usize) {
         lambda += 1;
     }
 
-    return (lambda, mu)
+    return (lambda, mu);
 }
 
 pub fn gcd(mut a: u64, mut b: u64) -> u64 {
     // Euclidean algorithm for determining the
     // Greatest Commen Divisor (GCD) of `a` and `b`
     while b != 0 {
-        (a, b) = (b, a%b);
+        (a, b) = (b, a % b);
     }
-    return a
+    return a;
 }
 
-pub fn lcm(a: u64, b:u64) -> u64 {
+pub fn lcm(a: u64, b: u64) -> u64 {
     // Least Common Multiple (LCM) of `a` and `b`
-    return (a / gcd(a, b)) * b
+    return (a / gcd(a, b)) * b;
 }
 
 pub fn bezout(a: i64, b: i64) -> (i64, i64, i64) {
     // Extended Euclidean algorithm for determining
     // Returns (gcd, s, t) such that Bezout's identity holds
     // gcd(a, b) = a*s + b*t
-    
+
     let mut r = (a, b);
     let mut s = (1_i64, 0_i64);
     let mut t = (0_i64, 1_i64);
     let mut q;
-    
+
     while r.1 != 0 {
         q = r.0 / r.1;
         r = (r.1, r.0 - q * r.1);
         s = (s.1, s.0 - q * s.1);
         t = (t.1, t.0 - q * t.1);
-    };
+    }
 
-    return (r.0, s.0, t.0)
+    return (r.0, s.0, t.0);
 }
 
 pub fn crt_solve(n_vec: &Vec<i64>, a_vec: &Vec<i64>) -> i64 {
@@ -77,20 +77,27 @@ pub fn crt_solve(n_vec: &Vec<i64>, a_vec: &Vec<i64>) -> i64 {
     // `n` and `a` must have same lengths
     // n_i's must be coprime
 
-    let mut eqs: Vec<(i64, i64)> = std::iter::zip(n_vec, a_vec).map(|(n, a)| (*n as i64, *a as i64)).collect();
+    let eqs: Vec<(i64, i64)> = std::iter::zip(n_vec, a_vec)
+        .map(|(n, a)| (*n as i64, *a as i64))
+        .collect();
     // eqs.sort_by_key(|(n, _)| Reverse(*n)); // do not know if sorting makes a difference
-    
+
     let (mut n0, mut a0) = (1, 0);
     for (n1, a1) in eqs {
         let (_, m0, m1) = bezout(n0, n1);
-        let x = a1*m0*n0 + a0*m1*n1;
+        let x = a1 * m0 * n0 + a0 * m1 * n1;
 
-        n0 = n0*n1;
+        n0 = n0 * n1;
         a0 = x % n0;
-    };
+    }
 
-    return if a0 >= 0 {a0} else {a0 + n0};
+    return if a0 >= 0 { a0 } else { a0 + n0 };
 }
+
+// struct Complex;
+
+// fn hermite_normal_form() {} // solving linear equation over integers: https://en.wikipedia.org/wiki/Hermite_normal_form
+//                             // maybe this is the alg to implement: https://sites.math.rutgers.edu/~sk1233/courses/ANT-F14/lec3.pdf
 
 #[cfg(test)]
 mod tests {
@@ -113,15 +120,15 @@ mod tests {
     fn test_bezout() {
         let (a, b) = (177741, 149553);
         let (gcd_, s, t) = bezout(a, b);
-        
+
         assert_eq!(gcd_, gcd(177741, 149553) as i64);
-        assert_eq!(gcd_, a*s + b*t);
+        assert_eq!(gcd_, a * s + b * t);
     }
 
     #[test]
     fn test_floyd() {
         let fun = |x: &i32| ((x + 2) % 64) as i32;
-        
+
         assert_eq!(floyd(1, &fun), (32, 0));
     }
 
