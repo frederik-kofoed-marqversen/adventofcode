6d7ab19cfb66def0c3b75a34c2a7e2a91caaaef2
frederik-kofoed-marqversen <frederik@marqversen.dk>
2024-12-06 12:37:28 +0100

    day06 renaming of variables and slight optimisation

--- 2024/day06/src/main.rs
+++ 2024/day06/src/main.rs
@@ -11,7 +11,7 @@ fn main() {
 
     // Record obstacle and start positions
     let bounds = [map.len() as i32, map[0].len() as i32];
-    let mut obstacles = HashSet::new();
+    let mut obstructions = HashSet::new();
     let mut start = I * 0;
     for (i, row) in map.iter().enumerate() {
         for (j, c) in row.iter().enumerate() {
@@ -19,57 +19,62 @@ fn main() {
             match c {
                 '^' => start = pos,
                 '#' => {
-                    obstacles.insert(pos);
+                    obstructions.insert(pos);
                 }
                 _ => {}
             }
         }
     }
 
-    // Function that simulates the guards path
-    let simulate = |obstruction: Option<Complex<i32>>| -> (HashSet<Complex<i32>>, bool) {
-        let mut dir = Complex::<i32> { real: -1, imag: 0 };
-        let mut guard = start;
-        let mut obsts = obstacles.clone();
-        if obstruction.is_some() {
-            obsts.insert(obstruction.unwrap());
-        }
-
-        let mut visited = HashSet::new();
-        let mut is_loop = false;
-        while [guard.real, guard.imag]
-            .iter()
-            .zip(bounds)
-            .all(|(&g, b)| g >= 0 && g < b)
-        {
-            let state = (guard, dir);
-            // loop detection
-            if visited.contains(&state) {
-                is_loop = true;
-                break;
+    // Function to simulate the guards path
+    let simulate =
+        |obstruction: Option<Complex<i32>>| -> (HashSet<(Complex<i32>, Complex<i32>)>, bool) {
+            // Add obstruction
+            let mut obsts = obstructions.clone();
+            if obstruction.is_some() {
+                obsts.insert(obstruction.unwrap());
             }
-            // record state
-            visited.insert(state);
 
-            // Take step
-            while obsts.contains(&(guard + dir)) {
-                dir = -I * dir;
+            // Guard starting state
+            let mut dir = Complex::<i32> { real: -1, imag: 0 };
+            let mut guard = start;
+
+            // Simulate
+            let mut guard_states = HashSet::new();
+            let mut is_loop = false;
+            while [guard.real, guard.imag]
+                .iter()
+                .zip(bounds)
+                .all(|(&g, b)| g >= 0 && g < b)
+            {
+                let state = (guard, dir);
+                // loop detection
+                if guard_states.contains(&state) {
+                    is_loop = true;
+                    break;
+                }
+                // record state
+                guard_states.insert(state);
+                // Take step
+                while obsts.contains(&(guard + dir)) {
+                    dir = -I * dir;
+                }
+                guard = guard + dir;
             }
-            guard = guard + dir;
-        }
 
-        let distinct_positions =
-            HashSet::<Complex<i32>>::from_iter(visited.iter().map(|(pos, _)| *pos));
-        return (distinct_positions, is_loop);
-    };
+            return (guard_states, is_loop);
+        };
 
     // PART 1
-    let mut guard_path = simulate(None).0;
-    println!("Result part 1: {}", guard_path.len());
+    let guard_states = simulate(None).0;
+    // Filter out states occupying the same positions
+    let mut distinct_positions: HashSet<Complex<i32>> =
+        HashSet::from_iter(guard_states.into_iter().map(|(pos, _)| pos));
+    println!("Result part 1: {}", distinct_positions.len());
 
     // PART 2
-    guard_path.remove(&start);
-    let loop_count = guard_path
+    distinct_positions.remove(&start);
+    let loop_count = distinct_positions
         .into_iter()
         .filter(|&pos| simulate(Some(pos)).1)
         .count();
